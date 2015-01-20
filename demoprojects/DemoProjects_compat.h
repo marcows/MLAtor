03fb120227003a66f7900c6116d4a6ff97223172
Markus Heidelberg <markus.heidelberg@web.de>
2015-01-20 02:09:06 +0100

    demo projects: support "PIC32 LCC"
    It uses the USE_ALPHABLEND_LITE feature. BarAlpha() is invoked
    indirectly from the alpha blended buttons.
    
    The "PIP" and "Paging" sub demos are only supported rudimentary without
    the special hardware features.

--- demoprojects/DemoProjects_compat.h
+++ demoprojects/DemoProjects_compat.h
@@ -1,11 +1,6 @@
 #ifndef _DEMO_PROJECTS_COMPAT_H
 #define _DEMO_PROJECTS_COMPAT_H
 
-// display layout, normally defined in HardwareProfile.h
-#define DISP_ORIENTATION    0
-#define DISP_HOR_RESOLUTION 320
-#define DISP_VER_RESOLUTION 240
-
 #if defined(TARGET_AppNotes)
   #define COMPAT_DUMMY_EVENT_POLL
   #define COMPAT_TICK_COUNTER
@@ -33,8 +28,26 @@
   #define GFX_PICTAIL_LCC
   #define COMPAT_TICK_COUNTER
 
+#elif defined(TARGET_PIC32LCC)
+  #define COMPAT_GENERAL_EVENTS_HOOK // for rendering during "Speed" sub demo
+  #define GFX_PICTAIL_V3
+  #define DISP_HOR_RESOLUTION 480
+  #define DISP_VER_RESOLUTION 272
+
 #elif defined(TARGET_PrimitiveLayer)
   #define COMPAT_GENERAL_EVENTS_HOOK
+#endif
+
+/* display layout, normally defined in HardwareProfile.h */
+
+#define DISP_ORIENTATION    0
+
+#ifndef DISP_HOR_RESOLUTION
+  #define DISP_HOR_RESOLUTION 320
+#endif
+
+#ifndef DISP_VER_RESOLUTION
+  #define DISP_VER_RESOLUTION 240
 #endif
 
 #endif
