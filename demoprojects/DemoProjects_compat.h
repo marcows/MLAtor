b5dec6b4c76d08ea91146698f3175a1e2e736593
Markus Heidelberg <markus.heidelberg@web.de>
2021-01-04 21:53:22 +0100

    Color Depth demo: do not define EXTERNAL_MEMORY as compile switch
    Simply define the real MLA compile switches directly in the Makefile
    instead of introducing a new one only used for this indirection.

--- demoprojects/DemoProjects_compat.h
+++ demoprojects/DemoProjects_compat.h
@@ -10,11 +10,6 @@
   #define CFG_INCLUDE_MPLAB_X
   #define Screen1_1BPP_Palette_SIZE Screen_1BPP_Palette_SIZE
   #define Screen2_1BPP_Palette_SIZE Screen_1BPP_Palette_SIZE
-  #ifdef EXTERNAL_MEMORY
-    #define USE_PALETTE_EXTERNAL
-    #define USE_FONT_EXTERNAL
-    #define USE_BITMAP_EXTERNAL
-  #endif
 
 #elif defined(TARGET_ExternalMemory)
   #define COMPAT_GENERAL_EVENTS_HOOK
